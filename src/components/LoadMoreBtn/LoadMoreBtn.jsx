347af89fca45bdb1814b368f10f77d84f0da2143
Melodica4585 <melodica4585@gmail.com>
2024-03-11 14:07:17 +0200

    added css

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -2,10 +2,10 @@ import css from './LoadMoreBtn.module.css';
 
 export const LoadMoreBtn = ({ onClick }) => {
   return (
-    <>
+    <div className={css.container}>
       <button className={css.button} onClick={onClick}>
         Load more images
       </button>
-    </>
+    </div>
   );
 };
